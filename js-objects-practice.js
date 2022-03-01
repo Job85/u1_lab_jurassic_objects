25c19a9b33361e0d2aa3787c3bc5735c251e4a77
Job85 <julianjernigan@ymail.com>
2022-03-01 11:34:49 -0500

    created first set of variables for Jurassic Park

---
+++ js-objects-practice.js
@@ -0,0 +1,27 @@
+// const blade = {
+//     title: 'Blade',
+//     director: 'Stephen Norrington',
+//     actors: ['Wesley Snipes', 'Stephen Dorff', 'Kris Kristofferson'],
+//     realeaseYear: 1998,
+//     duration: 120
+// }
+
+// // console.log(blade.title)
+// // console.log(blade.director)
+// // console.log(blade.realeaseYear)
+
+// blade.directorsCut = 145
+
+// console.log(blade)
+
+// const snakewaterMontana = {
+//     paleontologist: 'Dr. Alan Grant',
+//     depth: '10 meters',
+//     speciment: 'Velociraptor'
+// }
+
+// // console.log(snakewaterMontana)
+
+// let guestOfHonor = snakewaterMontana.paleontologist
+// let cleverGirl = snakewaterMontana.speciment
+// console.log(cleverGirl)
